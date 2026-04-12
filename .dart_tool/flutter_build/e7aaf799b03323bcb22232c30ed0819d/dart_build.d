091c88050e91e76a65e8327b91a5d1289ba22bc3
Deivy276 <deivy276@gmail.com>
2026-04-12 12:42:42 -0600

    Revert "Refactor dashboard polling, helpers drawer, and attachment preview"
    This reverts commit 3d34c7ebb52ffbebdd047dd2fdfbde9e1056a0cd.

--- .dart_tool/flutter_build/e7aaf799b03323bcb22232c30ed0819d/dart_build.d
+++
@@ -1,1 +0,0 @@
- C:\\Users\\DeivyRafaelPatinoUga\\anaconda3\\Atalaya\\Mobile\\.dart_tool\\flutter_build\\e7aaf799b03323bcb22232c30ed0819d\\dart_build_result.json: 
--- android/app/src/main/kotlin/com/example/atalaya_mobile/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.atalaya_mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
