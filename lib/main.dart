327c3a430825acbd96e69c1f985ed6cc65507531
Deivy276 <deivy276@gmail.com>
2026-04-17 22:36:58 -0600

    fix(app): clean main entrypoint and add MyApp wrapper

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'core/config/feature_flags.dart';
 import 'core/theme/pro_palette.dart';
 import 'presentation/screens/auth_gate_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const ProviderScope(child: AtalayaApp()));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ProviderScope(
+      child: AtalayaApp(),
+    );
+  }
 }
 
 class AtalayaApp extends StatelessWidget {
